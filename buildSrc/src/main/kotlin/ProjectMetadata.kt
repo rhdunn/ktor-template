444cd7e1721cca28c95e1576477c64e3f1c5c8e8
Reece H. Dunn <msclrhd@gmail.com>
2023-12-05 12:48:30 +0000

    Add a ProjectMetadata.Build.VersionTag property.

---
+++ buildSrc/src/main/kotlin/ProjectMetadata.kt
@@ -0,0 +1,17 @@
+// Copyright (C) 2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+
+/**
+ * Information about the project.
+ */
+@Suppress("ConstPropertyName")
+object ProjectMetadata {
+    /**
+     * Version information about the current build of the project.
+     */
+    object Build {
+        /**
+         * The semantic version of the current version.
+         */
+        const val VersionTag = "1.0.0" // TODO: Modify this property.
+    }
+}
