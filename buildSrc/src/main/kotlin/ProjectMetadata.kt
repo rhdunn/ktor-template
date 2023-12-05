4c305480372436f0b9c95fee5e5d6766c2da06ac
Reece H. Dunn <msclrhd@gmail.com>
2023-12-05 12:49:48 +0000

    Add a ProjectMetadata.Build.Type property.

--- buildSrc/src/main/kotlin/ProjectMetadata.kt
+++ buildSrc/src/main/kotlin/ProjectMetadata.kt
@@ -1,3 +1,5 @@
+import io.github.rhdunn.gradle.maven.BuildType
+
 // Copyright (C) 2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
 
 /**
@@ -13,5 +15,10 @@ object ProjectMetadata {
          * The semantic version of the current version.
          */
         const val VersionTag = "1.0.0" // TODO: Modify this property.
+
+        /**
+         * The build type of this project.
+         */
+        val Type = BuildType.Snapshot // TODO: Modify this property.
     }
 }
