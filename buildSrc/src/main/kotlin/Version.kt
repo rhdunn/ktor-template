d89fe692f12134f3aad088e25ecc49205870f46c
Reece H. Dunn <msclrhd@gmail.com>
2023-12-05 12:38:34 +0000

    Add a Version.Kotlin property.

---
+++ buildSrc/src/main/kotlin/Version.kt
@@ -0,0 +1,16 @@
+// Copyright (C) 2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+
+/**
+ * Versions of the various plugins and libraries used by the project.
+ */
+@Suppress("ConstPropertyName")
+object Version {
+    /**
+     * The version of the Kotlin compiler and runtime.
+     *
+     * `SPDX-License-Identifier: Apache-2.0`
+     *
+     * @see <a href="https://github.com/JetBrains/kotlin">https://github.com/JetBrains/kotlin</a>
+     */
+    const val Kotlin = "1.9.21"
+}
