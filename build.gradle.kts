710900cb3294ec18aa0c0746a676e28c93cbb233
Reece H. Dunn <msclrhd@gmail.com>
2023-12-05 12:40:53 +0000

    Add a dependency on the Kotlin JVM gradle plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
-allprojects {
-    repositories {
-        mavenLocal()
-        mavenCentral()
-    }
+// Copyright (C) 2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+
+plugins {
+    kotlin("jvm") version Version.Plugin.KotlinJvm
+}
+
+repositories {
+    mavenCentral()
 }
--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -13,4 +13,18 @@ object Version {
      * @see <a href="https://github.com/JetBrains/kotlin">https://github.com/JetBrains/kotlin</a>
      */
     const val Kotlin = "1.9.21"
+
+    /**
+     * Versions of the various plugins used by the project.
+     */
+    object Plugin {
+        /**
+         * The version of the `kotlin("jvm")` plugin.
+         *
+         * `SPDX-License-Identifier: Apache-2.0`
+         *
+         * @see <a href="https://github.com/JetBrains/kotlin/tree/master/libraries/tools/kotlin-gradle-plugin">https://github.com/JetBrains/kotlin/tree/master/libraries/tools/kotlin-gradle-plugin</a>
+         */
+        const val KotlinJvm = Kotlin
+    }
 }
