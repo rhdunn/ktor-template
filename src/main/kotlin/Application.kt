78e895f31e896b7912d225d2fbde36601637e590
Reece H. Dunn <msclrhd@gmail.com>
2023-12-05 23:34:37 +0000

    Don't print 'Ktor template' when starting the server.

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -5,8 +5,6 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main() {
-    println("Ktor template")
-
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
