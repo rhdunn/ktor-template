dd52ce1e3902ced1087fd3eb2c0bf43810c7d081
Reece H. Dunn <msclrhd@gmail.com>
2024-04-20 11:10:05 +0100

    Create a test transaction helper.

---
+++ src/test/kotlin/io/github/rhdunn/exposed/tests/TestDbTransaction.kt
@@ -0,0 +1,27 @@
+// Copyright (C) 2024 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+package io.github.rhdunn.exposed.tests
+
+import org.h2.jdbcx.JdbcDataSource
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.transactions.transaction
+
+interface TestDbTransaction {
+    val tables: Array<Table>
+
+    fun transaction(block: () -> Unit) {
+        val source = JdbcDataSource()
+        source.setUrl("jdbc:h2:mem:;DATABASE_TO_LOWER=TRUE")
+
+        // Keep a connection open for the duration of the test so that H2 does
+        // not delete the in-memory database.
+        source.connection.use {
+            val database = Database.connect(source)
+            transaction(database) {
+                SchemaUtils.create(*tables)
+                block()
+            }
+        }
+    }
+}
