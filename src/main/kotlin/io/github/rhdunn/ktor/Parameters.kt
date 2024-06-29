c4f027cc1ccd7168552a766903d2b028dd62942b
Reece H. Dunn <msclrhd@gmail.com>
2024-06-29 10:55:03 +0100

    Add a Parameters.getOrNull helper extension.

---
+++ src/main/kotlin/io/github/rhdunn/ktor/Parameters.kt
@@ -0,0 +1,29 @@
+// Copyright (C) 2024 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+package io.github.rhdunn.ktor
+
+import io.ktor.http.*
+import io.ktor.server.plugins.*
+import io.ktor.util.converters.*
+import io.ktor.util.reflect.*
+
+@Suppress("NOTHING_TO_INLINE")
+inline fun Parameters.getOrNull(name: String): String? {
+    return get(name)?.takeIf { it.isNotEmpty() }
+}
+
+inline fun <reified R : Any> Parameters.getOrNull(name: String): R? {
+    return getOrNullImpl(name, typeInfo<R>())
+}
+
+@PublishedApi
+internal fun <R : Any> Parameters.getOrNullImpl(name: String, typeInfo: TypeInfo): R? {
+    val values = getAll(name)
+        ?.filter { it.isNotEmpty() }
+        ?.takeIf { it.isNotEmpty() } ?: return null
+    return try {
+        @Suppress("UNCHECKED_CAST")
+        DefaultConversionService.fromValues(values, typeInfo) as R
+    } catch (cause: Exception) {
+        throw ParameterConversionException(name, typeInfo.type.simpleName ?: typeInfo.type.toString(), cause)
+    }
+}
