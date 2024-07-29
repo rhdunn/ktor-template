e06b3a090c7f79b251e1bef3e6fac52a382ff833
Reece H. Dunn <msclrhd@gmail.com>
2024-07-29 13:50:04 +0100

    Add a buildSchemaObject helper method.

--- src/main/kotlin/io/github/rhdunn/ktor/application/JsonResponse.kt
+++ src/main/kotlin/io/github/rhdunn/ktor/application/JsonResponse.kt
@@ -4,8 +4,7 @@ package io.github.rhdunn.ktor.application
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
-import kotlinx.serialization.json.JsonArray
-import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.*
 
 suspend fun ApplicationCall.respondJson(json: String, status: HttpStatusCode = HttpStatusCode.OK) {
     respondText(json, contentType = ContentType.Application.Json, status = status)
@@ -33,6 +32,19 @@ suspend fun ApplicationCall.respondJsonLd(json: JsonObject, status: HttpStatusCo
 @Suppress("unused")
 suspend fun ApplicationCall.respondJsonLd(json: JsonArray, status: HttpStatusCode = HttpStatusCode.OK) {
     respondJsonLd(json.toString(), status)
+}
+
+@Suppress("unused")
+fun buildSchemaObject(
+    type: String,
+    isRoot: Boolean = false,
+    builder: JsonObjectBuilder.() -> Unit
+): JsonObject = buildJsonObject {
+    if (isRoot) {
+        put("@context", JsonPrimitive("https://schema.org"))
+    }
+    put("@type", JsonPrimitive(type))
+    this.builder()
 }
 
 private val JsonLd = ContentType.parse("application/ld+json")
